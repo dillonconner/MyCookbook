6857c80cc9dd57d4cb98e7ea2acae7e96e5ba8f9
dillonconner <dillonconner@gmail.com>
2023-04-26 18:28:32 -0400

    made prettier

--- src/util/azureStorage.js
+++ src/util/azureStorage.js
@@ -2,7 +2,6 @@
 // To bad im using it anyway
 import { BlobServiceClient } from "@azure/storage-blob";
 
-//blob sas url = 'https://cookbookimages.blob.core.windows.net/images?sp=rad&st=2023-04-26T19:25:08Z&se=2023-05-04T03:25:08Z&spr=https&sv=2021-12-02&sr=c&sig=%2BZ26mAf0URfLQqxgGPpqIS7cpVvEPp4cqCDpKezXHbI%3D'
 const containerName = `images`;
 const sasToken = 'sp=racwd&st=2023-04-26T19:57:38Z&se=2023-04-27T03:57:38Z&spr=https&sv=2021-12-02&sr=c&sig=Cz3qKlSSZwxCad3jjMdYczwUOGRXVXyA0caqscxlexo%3D';
 const storageAccountName = 'cookbookimages';
@@ -17,18 +16,13 @@ export const isStorageConfigured = () => {
 };
 
 const createBlobInContainer = async (file, id) => {
-  // create blobClient for container
-  const blobClient = containerClient.getBlockBlobClient(id);
-  // set mimetype as determined from browser with file upload control
+  const blobClient = containerClient.getBlockBlobClient(id); // create blobClient for container
   const options = { blobHTTPHeaders: { blobContentType: file.type } };
-
-  // upload file
-  await blobClient.uploadData(file, options);
+  await blobClient.uploadData(file, options); // upload file
 };
 
 const uploadFileToBlob = async (file, id) => {
   if (!file) return;
-  // upload file
   await createBlobInContainer(file, id);
 };
 export const makeAzureUrl = (id) => `https://${storageAccountName}.blob.core.windows.net/${containerName}/${id}?${sasToken}`;
